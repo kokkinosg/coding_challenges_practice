33e0becbd6e5d7779871f99c036e570061dd51bd
Georgios Kokkinos <kokkinos_g@outlook.com>
2026-03-03 11:23:25 +0000

    Add optimal solution for best time to buy and sell stocks

--- LeetCode/Buy and sell stock/best_time_to_buy_and_sell_stock.java
+++ LeetCode/Buy and sell stock/best_time_to_buy_and_sell_stock.java
@@ -21,12 +21,38 @@ public class best_time_to_buy_and_sell_stock {
         return profit;
     }
 
+    // The theoory is that the biggest price is offered by the minimum and maximum prices
+    // I need to find the minimum price and max profit in a single pass
+
+    public int optimised_solution(int[] prices){
+
+        int min_price = Integer.MAX_VALUE;
+        int max_profit = 0;
+
+
+        for (int price:prices){
+            // Identify the min price seen so far
+            if (price<min_price){
+                min_price = price;
+            }
+            // Calculate profit based on current minimum price
+            int current_profit = price - min_price;
+
+            // Update max profit
+            if (current_profit>max_profit){
+                max_profit = current_profit;
+            }
+        }
+
+        return max_profit;
+    }
 
     public static void main(String[] args) {
         
-        int[] prices = {7,6,4,3,1};
+        int[] prices = {7,1,6,4,3,1};
         best_time_to_buy_and_sell_stock my_class = new best_time_to_buy_and_sell_stock();
         System.out.println(my_class.brute_solution(prices));
+        System.out.println(my_class.optimised_solution(prices));
     }
 
 }
