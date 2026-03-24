d3dbc3d6ed24e5db01537eb8bc3802ff0d5c062d
Georgios Kokkinos <kokkinos_g@outlook.com>
2026-03-24 20:12:38 +0000

    Add optimal solution to search arra problem

--- LeetCode/Array/Search in sorted array/search_array.java
+++ LeetCode/Array/Search in sorted array/search_array.java
@@ -3,59 +3,45 @@ public class search_array {
     //To solve this I will use binary search 
     public int solution(int[] nums, int target){
 
-        // Define the left, middle and right indexes
+        // Define the left and right indexes
         int left = 0;
-        int right = nums.length;
+        int right = nums.length - 1;
 
-        // Middle index needs to be relative to the left and right 
-        int middle = left + (right-left)/2;
+        // Because array is rotated, each time one half is sorted.
+        // Check whether the target lies in the sorted half.
+        // If yes, keep that half. Otherwise, keep the other half.
+        while (left <= right) {
 
-        // Because array is sorted, I just need each time to find out in which half of the array/subarray the number of interest is
-        // Every time i split hte array, I get a sorted subarray and an unsorted subarray. From the sorted, I need to check if my target is within 
-        // its left and right side, if not, split the unsorted. 
-        while (true) { 
-            // Check if the array has split enough times that the length of each half is 1
-            if (right - left == 1){
-                // Check if the target is the left element or right
-                if(nums[left] == target){
-                    return left; // return the index of the target
-                } else if (nums[right] == target){
-                    return right;
-                } else {
-                    // This means target is not in the array
-                    return -1;
-                }
+            // Middle index must always be recomputed inside the loop
+            int middle = left + (right - left) / 2;
+
+            // Check if middle is the target
+            if (nums[middle] == target) {
+                return middle;
             }
 
-            // Do the splitting 
-
             // Check if the left half is sorted
-            if(nums[left]<nums[middle]){
-                // Check if the target is within this sorted half which is the left half
-                if(target>=nums[left] && target <=nums[middle]){
-                    // Split the sorted half
-                    right = middle;
-                    middle = left + (right -left)/2;
+            if (nums[left] <= nums[middle]) {
+
+                // Check if target is inside the sorted left half
+                if (target >= nums[left] && target < nums[middle]) {
+                    right = middle - 1; // reduce the searh space otherwise we may get infinite loop
                 } else {
-                    // The target is in the unsorted right half. 
-                    left = middle;
-                    middle = left + (right-left)/2;
+                    left = middle + 1;
                 }
-                
-            } else { // The right half is sorted. 
-                
-                // Check if the target is within this right half
-                if(target>= middle && target <= nums[right]){
-                    // Split the sorted right half
-                    left = middle;
-                    middle = left + (right-left)/2;
+
+            } else { // The right half is sorted
+
+                // Check if target is inside the sorted right half
+                if (target > nums[middle] && target <= nums[right]) {
+                    left = middle + 1;
                 } else {
-                    // The target is in the unsorted left half. 
-                    right = middle;
-                    middle = left + (right-left)/2;
+                    right = middle - 1;
                 }
             }
         }
-    
+
+        // Target not found
+        return -1;
     }
 }
